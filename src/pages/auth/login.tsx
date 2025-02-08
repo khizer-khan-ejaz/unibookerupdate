5510d6bec3b129dfe79b706ff7cd327f791f25b3
khizer-khan-ejaz <khizerkhanejaz7860@gmail.com>
2025-02-07 19:17:36 -0500

    Fix dependency issue and update packages

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { Container, Row, Col } from "react-bootstrap";
-import Newsletter from "../components/common/Newsletter";
+
 import styles from "@/styles/Auth.module.css";
 import Image from "next/image";
 import loginimg from "../../Images/login.png";
