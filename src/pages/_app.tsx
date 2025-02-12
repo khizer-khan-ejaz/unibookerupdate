548305e72e0e560633d78c4b42258d62aebfa446
khizer-khan-ejaz <khizerkhanejaz7860@gmail.com>
2025-02-12 11:36:44 -0500

    new update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,51 +6,29 @@ import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import { AuthProvider } from "../context/AuthContext";
-import { useState, useEffect } from "react";
-import Router from "next/router";
-
 import { Jost } from "next/font/google";
-import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { SessionProvider } from "next-auth/react";
+
 const jostFont = Jost({
   variable: "--font-jost",
   subsets: ["latin"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  
-  useEffect(() => {
-    const handleRouteChangeStart = () => setLoading(true);
-    const handleRouteChangeComplete = () => setLoading(false);
-    const handleRouteChangeError = () => setLoading(false);
-    Router.events.on("routeChangeStart", handleRouteChangeStart);
-    Router.events.on("routeChangeComplete", handleRouteChangeComplete);
-    Router.events.on("routeChangeError", handleRouteChangeError);
-    return () => {
-      Router.events.off("routeChangeStart", handleRouteChangeStart);
-      Router.events.off("routeChangeComplete", handleRouteChangeComplete);
-      Router.events.off("routeChangeError", handleRouteChangeError);
-    };
-  }, []);
-
   return (
     <SessionProvider session={pageProps.session}>
-    <AuthProvider>
-      <ThemeProvider>
-        <Header />
-      
-          
-       
+      <AuthProvider>
+        <ThemeProvider>
+          <Header />
           <div className={jostFont.variable}>
             <Component {...pageProps} />
           </div>
-      
-        <Footer />
-        <ToastContainer />
-      </ThemeProvider>
-    </AuthProvider>
+          <Footer />
+          <ToastContainer />
+        </ThemeProvider>
+      </AuthProvider>
     </SessionProvider>
   );
 }
